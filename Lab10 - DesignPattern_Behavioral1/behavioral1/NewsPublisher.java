fa2a3c5a21db0681462ffd1b81ae40f9cad2aa52
Noppawat K <noppawatum@gmail.com>
2022-04-08 14:32:17 +0700

    upload files

--- Lab10 - DesignPattern_Behavioral1/behavioral1/NewsPublisher.java
+++ Lab10 - DesignPattern_Behavioral1/behavioral1/NewsPublisher.java
@@ -1,8 +1,22 @@
 package edu.parinya.softarchdesign.behavioral;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Flow.Publisher;
 import java.util.concurrent.Flow.Subscriber;
 
 public class NewsPublisher implements Publisher<News> {
 	// You need to implement this class
+    private List<Subscriber> subscribers = new ArrayList<>();
+
+    @Override
+    public void subscribe(Subscriber subscriber){
+        subscribers.add(subscriber);
+    }
+
+    public void publish(News news){
+        for(Subscriber subscriber:subscribers){
+            subscriber.onNext(news);
+        }
+    }
 }
--- Lab10 - DesignPattern_Behavioral1/behavioral1/NewsSubscriber.java
+++ Lab10 - DesignPattern_Behavioral1/behavioral1/NewsSubscriber.java
@@ -5,4 +5,27 @@ import java.util.concurrent.Flow.Subscription;
 
 public class NewsSubscriber implements Subscriber<News> {
 	// You need to implement this class
+    private String subscriber;
+
+    public NewsSubscriber(String subscriber){
+        this.subscriber = subscriber;
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription){
+    }
+
+    @Override
+    public void onNext(News news){
+        System.out.printf("Sending news (%s) to : %s \n",news.getContent(), subscriber);
+    }
+
+    @Override
+    public void onError(Throwable throwable){
+        System.out.printf("Error!");
+    }
+
+    @Override
+    public void onComplete(){
+    }
 }
